89f68a209a9ddf06ef5f24c4bdbf5d22a1cfc47c
Vele.Ndamulelo <velendamulelo057@gmail.com>
2026-02-26 14:58:52 +0200

    fixes: fixed type error

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,10 +10,10 @@ const api = axios.create({
 let isRefreshing = false;
 let failedQueue: Array<{
     resolve: (token: string) => void;
-    reject: (error: any) => void;
+    reject: (error: unknown) => void;
 }> = [];
 
-const processQueue = (error: any, token: string | null = null) => {
+const processQueue = (error: unknown, token: string | null = null) => {
     failedQueue.forEach(prom => {
         if (error) {
             prom.reject(error);
